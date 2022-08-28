191483349ba3da7e35bf9fd1776588b36fcc4c29
Hyunfor <navel5148@gmail.com>
2022-08-28 20:56:51 +0900

    게시물 작성과 리스팅

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("=== 프로그램 시작 ===");
+		
+		List<Article> articles = new ArrayList<>();
+		
+		int lastArticleId = 0;
 		
 		while(true) {
 			
@@ -17,6 +24,39 @@ public class Main {
 				break;
 			}
 			
+			if(cmd.equals("article write")) {
+				System.out.println("=== 게시물 작성 ===");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				System.out.println("title : " + title);
+				System.out.println("body : " + body);
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+				
+				continue;
+			} else if(cmd.equals("article list")) {
+				System.out.println("=== 게시물 리스트 ===");
+				
+				if(articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				
+				System.out.println("번호   |    제목");
+				
+				for(int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%4d   |   %s\n", article.id, article.title);
+				}
+				
+			}
 		}
 
 	}
